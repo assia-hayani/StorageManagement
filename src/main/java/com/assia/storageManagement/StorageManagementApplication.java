cf244a69548e2e1902d7089160beaf86044d7d13
assous <hayaniassia@gmail.com>
2021-09-17 15:21:23 +0100

    first project startup

---
+++ src/main/java/com/assia/storageManagement/StorageManagementApplication.java
@@ -0,0 +1,13 @@
+package com.assia.storageManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageManagementApplication.class, args);
+	}
+
+}
